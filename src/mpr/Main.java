6ea2cf5ce48185d82b1fc79d18fe333069aee366
ivanoreh456 <ivan.oreh@hotmail.com>
2014-06-11 17:07:07 +0200

    Main file -> algorithm + code

--- src/mpr/Main.java
+++ src/mpr/Main.java
@@ -1,8 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+﻿/*
+ 
+Creator: 
+Ivan Orehovec
+
  */
+
 
 package mpr;
 import java.io.File;
